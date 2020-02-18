e6260ddc9524f707255781451f86fb39431d38cf
ilmich <ardutu@gmail.com>
2020-02-18 22:09:03 +0100

    Added routing method

--- src/main/java/io/github/ilmich/tempesta/web/http/HttpHandlerFactory.java
+++ src/main/java/io/github/ilmich/tempesta/web/http/HttpHandlerFactory.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import javax.xml.ws.spi.http.HttpHandler;
+
 import io.github.ilmich.tempesta.configuration.AnnotationsScanner;
 import io.github.ilmich.tempesta.configuration.Configuration;
 import io.github.ilmich.tempesta.util.HttpUtil;
@@ -20,182 +22,184 @@ import io.github.ilmich.tempesta.web.http.auth.AuthHandler;
 
 public class HttpHandlerFactory implements HandlerFactory {
 
-    /**
-     * "Normal/Absolute" (non group capturing) RequestHandlers e.g. "/",
-     * "/persons"
-     */
-    private Map<String, HttpRequestHandler> absoluteHandlers = new HashMap<String, HttpRequestHandler>();
+	/**
+	 * "Normal/Absolute" (non group capturing) RequestHandlers e.g. "/", "/persons"
+	 */
+	private Map<String, HttpRequestHandler> absoluteHandlers = new HashMap<String, HttpRequestHandler>();
 
-    /**
-     * Group capturing RequestHandlers e.g. "/persons/([0-9]+)",
-     * "/persons/(\\d{1,3})"
-     */
-    private Map<String, HttpRequestHandler> capturingHandlers = new HashMap<String, HttpRequestHandler>();
+	/**
+	 * Group capturing RequestHandlers e.g. "/persons/([0-9]+)",
+	 * "/persons/(\\d{1,3})"
+	 */
+	private Map<String, HttpRequestHandler> capturingHandlers = new HashMap<String, HttpRequestHandler>();
 
-    /**
-     * A mapping between group capturing RequestHandlers and their corresponding
-     * pattern ( e.g. "([0-9]+)" )
-     */
-    private Map<HttpRequestHandler, Pattern> patterns = new HashMap<HttpRequestHandler, Pattern>();
+	/**
+	 * A mapping between group capturing RequestHandlers and their corresponding
+	 * pattern ( e.g. "([0-9]+)" )
+	 */
+	private Map<HttpRequestHandler, Pattern> patterns = new HashMap<HttpRequestHandler, Pattern>();
 
-    /**
-     * The directory where static content (files) will be served from.
-     */
-    private String staticContentDir;
+	/**
+	 * The directory where static content (files) will be served from.
+	 */
+	private String staticContentDir;
 
-    /**
-     * A copy of the <code>Configuration</code> used to create this type.
-     */
-    //private Configuration configuration;
+	/**
+	 * A copy of the <code>Configuration</code> used to create this type.
+	 */
+	// private Configuration configuration;
 
-    private AuthHandler authHandler;
+	private AuthHandler authHandler;
 
-    public HttpHandlerFactory() {
-	super();
-    }
-    
-    public HttpHandlerFactory(Configuration conf) {
-	
-	if (!Strings.isNullOrEmpty(conf.getHandlerPackage()))
-	    conf.addHandlers(new AnnotationsScanner().findHandlers(conf.getHandlerPackage()));
-
-	for (String path : conf.getHandlerMap().keySet()) {
-	    int index = path.lastIndexOf("/");
-	    String group = path.substring(index + 1, path.length());
-	    if (containsCapturingGroup(group)) {
-		// path ends with capturing group, e.g path ==
-		// "/person/([0-9]+)"
-	    capturingHandlers.put(path.substring(0, index + 1), conf.getHandlerMap().get(path));
-		patterns.put(conf.getHandlerMap().get(path), Pattern.compile(group));
-	    } else {
-		// "normal" path, e.g. path == "/"
-	    absoluteHandlers.put(path, conf.getHandlerMap().get(path));
-	    }
+	public HttpHandlerFactory() {
+		super();
 	}
-	
-	staticContentDir = conf.getStaticDirectory();
-    }
 
-    /**
-     * 
-     * @param path
-     *            Requested path
-     * @return Returns the {@link RequestHandler} associated with the given
-     *         path. If no mapping exists a {@link NotFoundRequestHandler} is
-     *         returned.
-     */
-    private HttpRequestHandler getHandler(String path) {
+	public HttpHandlerFactory(Configuration conf) {
 
-	HttpRequestHandler rh = absoluteHandlers.get(path);
-	if (rh == null) {
-	    rh = getCapturingHandler(path);
-	    if (rh == null) {
-		rh = getStaticContentHandler(path);
-		if (rh != null) {
-		    return rh;
+		if (!Strings.isNullOrEmpty(conf.getHandlerPackage()))
+			conf.addHandlers(new AnnotationsScanner().findHandlers(conf.getHandlerPackage()));
+
+		for (String path : conf.getHandlerMap().keySet()) {
+			addRoute(path, conf.getHandlerMap().get(path));
 		}
-	    } else {
-		return (rh.isSingleton()) ? rh : RequestHandlerFactory.cloneHandler(rh);
-	    }
-	} else {
-	    return (rh.isSingleton()) ? rh : RequestHandlerFactory.cloneHandler(rh);
+
+		staticContentDir = conf.getStaticDirectory();
+	}
+	
+	public HttpHandlerFactory addRoute(String path, HttpRequestHandler handler) {
+		int index = path.lastIndexOf("/");
+		String group = path.substring(index + 1, path.length());
+		if (containsCapturingGroup(group)) {
+			// path ends with capturing group, e.g path ==
+			// "/person/([0-9]+)"
+			capturingHandlers.put(path.substring(0, index + 1), handler);
+			patterns.put(handler, Pattern.compile(group));
+		} else {
+			// "normal" path, e.g. path == "/"
+			absoluteHandlers.put(path, handler);
+		}
+		return this;
 	}
 
-	return NotFoundRequestHandler.getInstance();
-    }
+	/**
+	 * 
+	 * @param path Requested path
+	 * @return Returns the {@link RequestHandler} associated with the given path. If
+	 *         no mapping exists a {@link NotFoundRequestHandler} is returned.
+	 */
+	private HttpRequestHandler getHandler(String path) {
 
-    public RequestHandler getHandler(Request request) {
+		HttpRequestHandler rh = absoluteHandlers.get(path);
+		if (rh == null) {
+			rh = getCapturingHandler(path);
+			if (rh == null) {
+				rh = getStaticContentHandler(path);
+				if (rh != null) {
+					return rh;
+				}
+			} else {
+				return (rh.isSingleton()) ? rh : RequestHandlerFactory.cloneHandler(rh);
+			}
+		} else {
+			return (rh.isSingleton()) ? rh : RequestHandlerFactory.cloneHandler(rh);
+		}
 
-	if (!HttpUtil.verifyRequest(request)) {
-	    return BadRequestRequestHandler.getInstance();
-	}
-	// if @Authenticated annotation is present, make sure that the
-	// request/user is authenticated
-	// (i.e RequestHandler.getCurrentUser() != null).
-	HttpRequestHandler rh = getHandler(request.getRequestedPath());	
-	if (rh == null) {
 		return NotFoundRequestHandler.getInstance();
 	}
-	rh.setAuthHandler(getAuthHandler());	
-	if (rh.isMethodAuthenticated(request.getMethod())
-		&& (getAuthHandler() == null || !getAuthHandler().isAuthorizedRequest(request))) {	    
-	    return getAuthHandler() == null ? new UnAuthorizedBasicRequestHandler("Unknown") : 
-					      getAuthHandler().getUnAuthorizedRequestHandler(request);
-	}
 
-	if (request.expectContinue()) {
-	    return HttpContinueRequestHandler.getInstance();
-	}
+	public RequestHandler getHandler(Request request) {
 
-	return rh;
-    }
-
-    private static boolean containsCapturingGroup(String group) {
-	boolean containsGroup = group.matches("^\\(.*\\)$");
-	Pattern.compile(group); // throws PatternSyntaxException if group is
-				// malformed regular expression
-	return containsGroup;
-    }
-
-    private HttpRequestHandler getCapturingHandler(String path) {
-	int index = path.lastIndexOf("/");
-	if (index != -1) {
-	    String init = path.substring(0, index + 1); // path without its last
-							// segment
-	    String group = path.substring(index + 1, path.length());
-	    HttpRequestHandler handler = capturingHandlers.get(init);
-	    if (handler != null) {
-		Pattern regex = patterns.get(handler);
-		if (regex.matcher(group).matches()) {
-		    return handler;
+		if (!HttpUtil.verifyRequest(request)) {
+			return BadRequestRequestHandler.getInstance();
 		}
-	    }
+		// if @Authenticated annotation is present, make sure that the
+		// request/user is authenticated
+		// (i.e RequestHandler.getCurrentUser() != null).
+		HttpRequestHandler rh = getHandler(request.getRequestedPath());
+		if (rh == null) {
+			return NotFoundRequestHandler.getInstance();
+		}
+		rh.setAuthHandler(getAuthHandler());
+		if (rh.isMethodAuthenticated(request.getMethod())
+				&& (getAuthHandler() == null || !getAuthHandler().isAuthorizedRequest(request))) {
+			return getAuthHandler() == null ? new UnAuthorizedBasicRequestHandler("Unknown")
+					: getAuthHandler().getUnAuthorizedRequestHandler(request);
+		}
+
+		if (request.expectContinue()) {
+			return HttpContinueRequestHandler.getInstance();
+		}
+
+		return rh;
 	}
-	return null;
-    }
 
-    protected HttpRequestHandler getStaticContentHandler(String path) {
-	if (staticContentDir == null || path.length() <= staticContentDir.length()) {
-	    return null; // quick reject (no static dir or simple contradiction)
+	private static boolean containsCapturingGroup(String group) {
+		boolean containsGroup = group.matches("^\\(.*\\)$");
+		Pattern.compile(group); // throws PatternSyntaxException if group is
+		// malformed regular expression
+		return containsGroup;
 	}
 
-	if (path.substring(1).startsWith(staticContentDir)) {
-	    return StaticContentHandler.getInstance();
-	} else {
-	    return null;
+	private HttpRequestHandler getCapturingHandler(String path) {
+		int index = path.lastIndexOf("/");
+		if (index != -1) {
+			String init = path.substring(0, index + 1); // path without its last
+			// segment
+			String group = path.substring(index + 1, path.length());
+			HttpRequestHandler handler = capturingHandlers.get(init);
+			if (handler != null) {
+				Pattern regex = patterns.get(handler);
+				if (regex.matcher(group).matches()) {
+					return handler;
+				}
+			}
+		}
+		return null;
 	}
-    }
 
-    void setStaticContentDir(String scd) {
-	staticContentDir = scd;
-    }
+	protected HttpRequestHandler getStaticContentHandler(String path) {
+		if (staticContentDir == null || path.length() <= staticContentDir.length()) {
+			return null; // quick reject (no static dir or simple contradiction)
+		}
 
-    /**
-     * Set the <code>Configuration</code> for use with this type.
-     * 
-     * @param configuration
-     *            the <code>Configuration</code> to apply.
-     */
-    /*public void setConfiguration(Configuration configuration) {
-	this.configuration = configuration;
-    }*/
+		if (path.substring(1).startsWith(staticContentDir)) {
+			return StaticContentHandler.getInstance();
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     * Retrieve the <code>Configuration</code> used by this type.
-     * 
-     * @return the current <code>Configuration</code>.
-     */
-    /*public Configuration getConfiguration() {
-	return configuration;
-    }*/
-    
-    public AuthHandler getAuthHandler() {
-	return authHandler;
-    }
+	void setStaticContentDir(String scd) {
+		staticContentDir = scd;
+	}
 
-    public HttpHandlerFactory setAuthHandler(AuthHandler authHandler) {
-	this.authHandler = authHandler;
-	return this;
-    }
+	/**
+	 * Set the <code>Configuration</code> for use with this type.
+	 * 
+	 * @param configuration the <code>Configuration</code> to apply.
+	 */
+	/*
+	 * public void setConfiguration(Configuration configuration) {
+	 * this.configuration = configuration; }
+	 */
+
+	/**
+	 * Retrieve the <code>Configuration</code> used by this type.
+	 * 
+	 * @return the current <code>Configuration</code>.
+	 */
+	/*
+	 * public Configuration getConfiguration() { return configuration; }
+	 */
+
+	public AuthHandler getAuthHandler() {
+		return authHandler;
+	}
+
+	public HttpHandlerFactory setAuthHandler(AuthHandler authHandler) {
+		this.authHandler = authHandler;
+		return this;
+	}
 
 }
